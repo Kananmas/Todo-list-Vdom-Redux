56ea0b5ba5a693b16ff135afb2c4eee08654a68b
Kananmas <99276326+Kananmas@users.noreply.github.com>
2022-12-29 22:03:09 +0330

    Update createCopyElement.js

--- Scripts/createCopyElement.js
+++ Scripts/createCopyElement.js
@@ -13,6 +13,7 @@ export default function createCopyElement(el = document.createElement('div')) {
     else {
         copyEl.textContent = el.textContent
     }
+
     for (let event of events) {
         if (el[event]) {
             copyEl[event] = el[event]
